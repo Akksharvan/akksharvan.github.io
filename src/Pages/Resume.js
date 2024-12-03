0b6c76e6743a5d4e1cd6102ddaf219aeb14b42f5
Akksharvan Senthilkumar <108592251+Akksharvan@users.noreply.github.com>
2024-12-02 21:40:51 -0500

    Work in Progress

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -1,10 +1,12 @@
+import resume from '../Data/resume.pdf';
+
 function Resume() {
   return (
     <section className="p-8 bg-gray-50">
       <h2 className="text-3xl font-bold text-primary">Resume</h2>
-      <p className="mt-4 text-lg text-gray-700">
-        Include your resume details here.
-      </p>
+      <a href={resume} target="_blank" rel="noopener noreferrer" className="text-blue-500">
+        View Resume (PDF)
+      </a>
     </section>
   );
 }
